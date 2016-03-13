5ee7d5b4203d22837025cc673015db8d5c70cf00
Christoph Aldrian <christoph.aldrian@gmail.com>
2016-03-13 15:19:44 +0100

    Change to also build shared dll

--- make.bat
+++ make.bat
@@ -5,12 +5,12 @@ copy zconf.h "..\zlib-1.2.8"
 cd ..
 
 cd lpng1621-build
-cmake -G "NMake Makefiles" "..\lpng1621" -DCMAKE_BUILD_TYPE=Release -DPNG_SHARED=OFF -DCMAKE_COLOR_MAKEFILE=ON -DZLIB_INCLUDE_DIR="..\zlib-1.2.8" -DZLIB_LIBRARY="..\..\zlib-1.2.8-build\zlib.lib"
+cmake -G "NMake Makefiles" "..\lpng1621" -DCMAKE_BUILD_TYPE=Release -DCMAKE_COLOR_MAKEFILE=ON -DZLIB_INCLUDE_DIR="..\zlib-1.2.8" -DZLIB_LIBRARY="..\zlib-1.2.8-build\zlib.lib"
 nmake
 copy pnglibconf.h "..\lpng1621"
 cd ..
 
 cd libharu-build
-cmake -G "NMake Makefiles" "..\libharu-RELEASE_2_3_0" -DCMAKE_BUILD_TYPE=Release -DBUILD_SHARED_LIBS=OFF -DCMAKE_COLOR_MAKEFILE=ON -DCMAKE_VERBOSE_MAKEFILE=OFF -DLIBHPDF_EXAMPLES=ON -DZLIB_INCLUDE_DIR="..\zlib-1.2.8" -DZLIB_LIBRARY="..\..\zlib-1.2.8-build\zlib.lib" -DPNG_PNG_INCLUDE_DIR="..\lpng1621" -DPNG_LIBRARY="..\..\lpng1621-build\libpng16_static.lib"
+cmake -G "NMake Makefiles" "..\libharu-RELEASE_2_3_0" -DCMAKE_BUILD_TYPE=Release -DCMAKE_COLOR_MAKEFILE=ON -DCMAKE_VERBOSE_MAKEFILE=OFF -DLIBHPDF_EXAMPLES=ON -DZLIB_INCLUDE_DIR="..\zlib-1.2.8" -DZLIB_LIBRARY="..\..\zlib-1.2.8-build\zlib.lib" -DPNG_PNG_INCLUDE_DIR="..\lpng1621" -DPNG_LIBRARY="..\..\lpng1621-build\libpng16_static.lib"
 nmake
 cd ..
